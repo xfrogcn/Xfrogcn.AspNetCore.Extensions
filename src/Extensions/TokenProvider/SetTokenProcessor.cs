030ac2386f9e6b369c0ebe6a66b6f7f7fe62d649
xfrogcn <xfrogcn@163.com>
2020-09-13 23:37:52 +0800

    令牌消息处理器可指定如何设置令牌

---
+++ src/Extensions/TokenProvider/SetTokenProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Xfrogcn.AspNetCore.Extensions.TokenProvider
+{
+    public class SetTokenProcessor
+    {
+        public SetTokenProcessor()
+        {
+        }
+    }
+}
