62930e09bb48920f957bb9fab5eadba35f66d088
xfrogcn <xfrogcn@163.com>
2020-10-07 16:28:19 +0800

    增加纯数字随机字符串

--- src/Extensions/Utils/StringExtensions.cs
+++ src/Extensions/Utils/StringExtensions.cs
@@ -9,9 +9,16 @@ namespace Xfrogcn.AspNetCore.Extensions
     {
         private static Random random = new Random();
         const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+        const string numbericChars = "0123456789";
         public static string RandomString(int length)
         {
             return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string RandomNumbericString(int length)
+        {
+            return new string(Enumerable.Repeat(numbericChars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
