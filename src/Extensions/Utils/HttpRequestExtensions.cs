bd05c393938afc0830bf8ef041986264b9289ae0
xfrogcn <xfrogcn@163.com>
2020-10-21 00:15:48 +0800

    请求中获取全Uri

---
+++ src/Extensions/Utils/HttpRequestExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace Xfrogcn.AspNetCore.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static string GetAbsoluteUri(this HttpRequest request)
+        {
+            return new StringBuilder()
+                .Append(request.Scheme)
+                .Append("://")
+                .Append(request.Host)
+                .Append(request.PathBase)
+                .Append(request.Path)
+                .Append(request.QueryString)
+                .ToString();
+        }
+    }
+}
