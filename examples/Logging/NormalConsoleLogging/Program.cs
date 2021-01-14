8a183fbafed04cf06974840a6b3e425cae453913
xfrogcn <xfrogcn@163.com>
2021-01-14 14:03:41 +0800

    日志级别的动态调整

--- examples/Logging/NormalConsoleLogging/Program.cs
+++ examples/Logging/NormalConsoleLogging/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Xfrogcn.AspNetCore.Extensions;
 
 namespace NormalConsoleLogging
 {
@@ -23,6 +24,17 @@ namespace NormalConsoleLogging
 
             var logger = host.Services.GetRequiredService<ILogger<Program>>();
             logger.LogInformation("测试日志：{time}", DateTimeOffset.Now);
+
+            // 动态修改日志级别
+            var apiConfig = host.Services.GetRequiredService<WebApiConfig>();
+            apiConfig.AppLogLevel = Serilog.Events.LogEventLevel.Error;
+            // 此日志被忽略
+            logger.LogInformation("测试日志：{time}", DateTimeOffset.Now);
+            apiConfig.AppLogLevel = Serilog.Events.LogEventLevel.Verbose;
+
+            // 日志显示
+            logger.LogInformation("测试日志：{time}", DateTimeOffset.Now);
+
 
             Console.ReadLine();
 
